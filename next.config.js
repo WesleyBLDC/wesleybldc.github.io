0808ecb95d965dd246275901d7834615aa550a11
WesleyBLDC <wezzdc@gmail.com>
2023-03-28 17:19:38 +0800

    somechanges

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
 const nextConfig = {
-  reactStrictMode: true,
+  // other valid and allowed properties
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = {
+        type: "filesystem",
+        buildDependencies: {
+          config: [__filename],
+        },
+      };
+    }
+
+    return config;
+  },
 };
+
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        // image optimization options
+      },
+    ],
+  ],
+  nextConfig
+);
 
 module.exports = nextConfig;
