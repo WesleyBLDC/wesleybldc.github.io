ef64d59aba4e0e915f0b0b1a58e764da6e3702fa
WesleyBLDC <wezzdc@gmail.com>
2023-03-28 17:54:23 +0800

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+// import Header from "../components/Header";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
